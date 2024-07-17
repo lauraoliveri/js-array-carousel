63b14dd7fb0dcb9a69b676a978e9f8ac61d11ec0
lauraoliveri <lauraoliveri2003@gmail.com>
2024-07-17 23:43:41 +0200

    Idk

---
+++ js/scripts.js
@@ -0,0 +1,36 @@
+// Milestone 2 
+
+// creo l'array
+const Images = [
+    './js/img/01.webp',
+    './js/img/02.webp',
+    './js/img/03.webp',
+    './js/img/04.webp',
+    './js/img/05.webp'
+]
+
+Container.innerHTML = (`
+    
+    
+    
+    `
+
+)
+
+const Container = document.createElement('div');
+
+for ( i = 0; i < 6; i++) {
+    const Container = document.createElement('div');
+
+    for ( Images [i] = 0; i < 6; i++ ) {
+        Container.innerHTML = Images;
+    }
+}
+
+Container.innerHTML = (`
+    
+    
+    
+    `)
+
+
